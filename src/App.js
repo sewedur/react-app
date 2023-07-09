d8150614337dd92b802607df8b1b3b69beb12962
sewedur <kushaluamos@gmail.com>
2023-07-08 22:43:32 -0600

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const [title, setTitle] = useState("");
+
+  function addTodo() {
+    setTodos([{text: todo, isCompleted: false}, ...todos]);
+    setTodo("");
+  }
+
+  function toggleComplete(index) {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  }
+
+  function deleteTodo(index) {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Enter Todo List Title..." value={title} onChange={e => setTitle(e.target.value)} />
+      <form onSubmit={e => {
+        e.preventDefault();
+        addTodo();
+      }}>
+        <input type="text" value={todo} onChange={e => setTodo(e.target.value)} />
+        <button type="submit">Add</button>
+      </form>
+      <h1>{title}</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Todo</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo.text}</td>
+              <td>{todo.isCompleted ? "Completed" : "In Progress"}</td>
+              <td>
+                <button onClick={() => toggleComplete(index)}>{todo.isCompleted ? "Undo" : "Complete"}</button>
+                <button onClick={() => deleteTodo(index)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
