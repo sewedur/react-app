8249b9bf1e79cadbcadbf7fa58d0a6161eeed8ed
sewedur <kushaluamos@gmail.com>
2023-07-08 22:43:56 -0600

    added git deployment

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
   const [title, setTitle] = useState("");
 
   function addTodo() {
-    setTodos([{text: todo, isCompleted: false}, ...todos]);
+    setTodos([{text: todo, status: "Pending"}, ...todos]);
     setTodo("");
   }
 
-  function toggleComplete(index) {
+  function updateStatus(index, status) {
     const newTodos = [...todos];
-    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    newTodos[index].status = status;
     setTodos(newTodos);
   }
 
@@ -25,15 +25,17 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Todo List App</h1>
+      
       <input type="text" placeholder="Enter Todo List Title..." value={title} onChange={e => setTitle(e.target.value)} />
+      <h2>{title}</h2>
       <form onSubmit={e => {
         e.preventDefault();
         addTodo();
       }}>
-        <input type="text" value={todo} onChange={e => setTodo(e.target.value)} />
-        <button type="submit">Add</button>
+        <input type="text" placeholder="Enter Todo..." value={todo} onChange={e => setTodo(e.target.value)} />
+        <button type="submit">Add Todo</button>
       </form>
-      <h1>{title}</h1>
       <table>
         <thead>
           <tr>
@@ -46,9 +48,14 @@ function App() {
           {todos.map((todo, index) => (
             <tr key={index}>
               <td>{todo.text}</td>
-              <td>{todo.isCompleted ? "Completed" : "In Progress"}</td>
               <td>
-                <button onClick={() => toggleComplete(index)}>{todo.isCompleted ? "Undo" : "Complete"}</button>
+                <select value={todo.status} onChange={e => updateStatus(index, e.target.value)}>
+                  <option value="Pending">Pending</option>
+                  <option value="In Progress">In Progress</option>
+                  <option value="Completed">Completed</option>
+                </select>
+              </td>
+              <td>
                 <button onClick={() => deleteTodo(index)}>Delete</button>
               </td>
             </tr>
